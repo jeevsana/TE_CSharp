874b960faa66f5fe0051b85c1c378abe4453602d
Jeevana <jeevsana@gmail.com>
2022-09-28 14:13:10 +0530

    Add project files.

---
+++ Enumeration.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TE_CSharp
+{
+
+    internal class Enumeration
+    {
+
+        enum Student
+        {
+            Sam,
+            Demon = 1051,
+            Jack = 1052,
+            Wakas = 1053,
+        }
+        public static void Main()
+        {
+            Console.WriteLine(Student.Sam);
+            int methods = (int)Student.Demon;
+            Console.WriteLine(methods);
+          
+            var del = (Student)1053;
+            Console.WriteLine(del);
+            int id = (int)Student.Jack;
+            Console.WriteLine(id);
+
+        }
+    }
+}
